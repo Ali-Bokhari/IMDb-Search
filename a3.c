eb2ad5eaae8472d09145908466003024ca1f07e7
Ali Bokhari <abokhari@uoguelph.ca>
2019-11-03 23:40:38 -0500

    Started interface

--- a3.c
+++ a3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "name.h"
 #include "common.h"
@@ -15,11 +16,59 @@ int main(int argc, char * argv[]) {
   struct name_basics *name;
   struct title_principals_array *title_principals;
   struct title_principals *principals;
+  char first[20];
+  char second[20];
   if (argc != 2) {
     fprintf( stderr, "Usage:  %s directory\n", argv[0] );
     exit(-1);
   }
+
   title_basics = get_title( argv[1] );
+
+  build_ptindex( title_basics );
+
+  build_tindex( title_basics );
+
+  printf("lines: %d\n", title_basics->num);
+
+  name_basics = get_name( argv[1] );
+
+
+  build_pnindex( name_basics );
+
+  build_nindex( name_basics );
+
+  printf("lines: %d\n", name_basics->num);
+
+  title_principals = get_principals( argv[1] );
+
+
+  build_pntindex( title_principals );
+
+  build_pncindex( title_principals );
+
+  printf("lines: %d\n", title_principals->num);
+
+  while (1) {
+    printf("> ");
+    scanf("%5s %100s", first, second);
+    printf("%s: %s\n", first, second);
+
+    if (strcmp(first, "name") == 0) {
+      name = find_primary_name( name_basics, second );
+      principals = find_principals_nconst( title_principals, name->nconst );
+      title = find_primary_title( title_basics, principals->tconst );
+      printf( "%s\n", title->primaryTitle );
+    } else if (strcmp(first, "title") == 0) {
+      title = find_primary_title (title_basics, second);
+      principals = find_principals_tconst(title_principals, title->tconst);
+      name = find_primary_name (name_basics, principals->nconst);
+      printf( "%s\n", name->primaryName );
+    }
+  }
+
+
+  /*title_basics = get_title( argv[1] );
 
   build_ptindex( title_basics );
 
@@ -54,7 +103,7 @@ int main(int argc, char * argv[]) {
 
   title = find_tconst( title_basics, principals->tconst );
 
-  printf( "%s\n", title->primaryTitle );
+  printf( "%s\n", title->primaryTitle );*/
 
   return 0;
 }
