397e1d7696d4225b993587dbb1593cbe44540a7a
Ali Bokhari <abokhari@uoguelph.ca>
2019-11-06 19:39:42 -0500

    Fixed main program implementation incase there is a remark

--- a3.c
+++ a3.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "name.h"
 #include "common.h"
 #include "title.h"
 #include "principals.h"
 #include "binary.h"
+
+void printing_name(struct node *tree, struct name_basics_array *name, char* tconstt) {
+  struct name_basics *nameb;
+  if (!tree) {
+    return;
+  }
+  if (strcmp (tconstt, ((struct title_principals*)tree->data)->tconst) == 0) {
+    nameb = find_nconst (name, ((struct title_principals*)tree->data)->nconst);
+    if (nameb) {
+      printf( "%s : %s]\n", nameb->primaryName, ((struct title_principals*)tree->data)->characters );
+    }
+  }
+  printing_name(tree->child[0], name, tconstt);
+  printing_name(tree->child[1], name, tconstt);
+}
+
+void printing_title(struct node *tree, struct title_basics_array *title, char* nconstt) {
+  struct title_basics *titleb;
+  if (!tree) {
+    return;
+  }
+  if (strcmp (nconstt, ((struct title_principals*)tree->data)->nconst) == 0) {
+    titleb = find_tconst (title, ((struct title_principals*)tree->data)->tconst);
+    if (titleb) {
+      printf( "%s : %s]\n", titleb->primaryTitle, ((struct title_principals*)tree->data)->characters );
+    }
+  }
+  printing_title(tree->child[0], title, nconstt);
+  printing_title(tree->child[1], title, nconstt);
+}
 
 
 int main(int argc, char * argv[]) {
@@ -15,9 +46,10 @@ int main(int argc, char * argv[]) {
   struct name_basics_array *name_basics;
   struct name_basics *name;
   struct title_principals_array *title_principals;
-  struct title_principals *principals;
-  char first[20];
-  char second[20];
+  /*struct title_principals *principals;*/
+  char first[10];
+  char second[256];
+  int command;
   if (argc != 2) {
     fprintf( stderr, "Usage:  %s directory\n", argv[0] );
     exit(-1);
@@ -45,58 +77,20 @@ int main(int argc, char * argv[]) {
 
   while (1) {
     printf("> ");
-    scanf("%5s %100s", first, second);
-
+    scanf("%s ", first);
+    fgets(second, 256, stdin);
+    for (command = strlen(second)-1; isspace(second[command]); command--);
+    second[command + 1] = '\0';
+    
     if (strcmp(first, "name") == 0) {
       name = find_primary_name( name_basics, second );
-      principals = find_principals_nconst( title_principals, name->nconst );
-      title = find_primary_title( title_basics, principals->tconst );
-      printf( "%s\n", title->primaryTitle );
+      if(!name){continue;}
+      printing_title(find_node(title_principals->nconst_tree, name->nconst), title_basics, name->nconst);
     } else if (strcmp(first, "title") == 0) {
       title = find_primary_title (title_basics, second);
-      principals = find_principals_tconst(title_principals, title->tconst);
-      name = find_primary_name (name_basics, principals->nconst);
-      printf( "%s\n", name->primaryName );
+      if(!title){continue;}
+      printing_name(find_node(title_principals->tconst_tree, title->tconst), name_basics, title->tconst);
     }
   }
-
-
-  /*title_basics = get_title( argv[1] );
-
-  build_ptindex( title_basics );
-
-  build_tindex( title_basics );
-
-  printf("lines: %d\n", title_basics->num);
-
-  name_basics = get_name( argv[1] );
-
-
-  build_pnindex( name_basics );
-
-  build_nindex( name_basics );
-
-  printf("lines: %d\n", name_basics->num);
-
-  title_principals = get_principals( argv[1] );
-
-
-  build_pntindex( title_principals );
-
-  build_pncindex( title_principals );
-
-  printf("lines: %d\n", title_principals->num);
-
-
-  printf( "Ready\n" );
-
-  name = find_primary_name( name_basics, "Bruce Lee" );
-
-  principals = find_principals_nconst( title_principals, name->nconst );
-
-  title = find_tconst( title_basics, principals->tconst );
-
-  printf( "%s\n", title->primaryTitle );*/
-
   return 0;
 }
